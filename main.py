2eac821ffa99df50abbaad014e978bc35d14154c
sman123123 <stuartmansur@gmail.com>
2020-12-17 16:08:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app.secret_key = "Some Secret Key"
 app.permanent_session_lifetime = timedelta(days=1)
 
 @app.route("/")
-def home(name):
-	return render_template("index.html", content=name)
+def home():
+	return render_template("base.html")
 
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
