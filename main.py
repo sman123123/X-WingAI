e17fbeb91edc86c2818ed14c464300b86bb709dd
sman123123 <stuartmansur@gmail.com>
2020-12-17 16:02:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+import sqlalchemy
 
 app = Flask(__name__)
+app.secret_key = "Some Secret Key"
+app.permanent_session_lifetime = timedelta(days=1)
 
 @app.route("/")
-def home():
-	return render_template("")
+def home(name):
+	return render_template("index.html", content=name)
 
 
 if __name__ == "__main__":
