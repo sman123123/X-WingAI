80d5cbb2288883d3e8617f63e7fa5da0c5c30180
sman123123 <stuartmansur@gmail.com>
2020-12-16 21:25:01 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+	return render_template("")
+
+
+if __name__ == "__main__":
+	app.run()
