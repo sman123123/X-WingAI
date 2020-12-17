519f444353217b89cb2cc2b77306dd93fdbb157d
sman123123 <stuartmansur@gmail.com>
2020-12-17 16:09:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.permanent_session_lifetime = timedelta(days=1)
 
 @app.route("/")
 def home():
-	return render_template("base.html")
+	return render_template("index.html")
 
 
 if __name__ == "__main__":
