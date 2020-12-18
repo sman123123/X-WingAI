fcd9550ca62507ec897be14652c0bc7bb5c0528e
sman123123 <stuartmansur@gmail.com>
2020-12-17 22:50:37 -0700

    changin stuff...

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from flask import Flask, redirect, url_for, render_template, request, session, flash
-from datetime import timedelta
+from flask import Flask, redirect, url_for, render_template
+from second import second
+import os
 
 app = Flask(__name__)
-app.secret_key = "Some Secret Key"
-app.permanent_session_lifetime = timedelta(days=1)
+app.register_blueprint(second, url_prefix="")
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 @app.route("/home")
 @app.route("/")
