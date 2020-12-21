744accc90bb1cb95fdab41e4417c4d27a81f603d
sman123123 <stuartmansur@gmail.com>
2020-12-20 23:03:00 -0700

    began login stuff

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
 from probability import Probability
 #from second import second
 import os
 
+
 app = Flask(__name__)
 #app.register_blueprint(second, url_prefix="")
+app.secret_key = "Stuart is smrat"
+app.permanent_session_lifetime = timedelta(days=1)
 
 @app.context_processor
 def override_url_for():
@@ -28,7 +32,9 @@ def index():
 def probabilityPage():
 	if request.method == "POST":
 		n = int(request.form["n"])
+		n = 10 if n > 10 else n
 		r = int(request.form["r"])
+		r = 10 if r > 10 else n
 		p = request.form["p"]
 		sp = p.split("/")
 		sp = int(sp[0])/int(sp[1])
@@ -40,6 +46,32 @@ def probabilityPage():
 @app.route("/construction")
 def construction():
 	return render_template("construction.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+	if request.method == "POST":
+		user = request.form["nm"]
+		session["user"] = user
+		session.permanent = True
+		return redirect(url_for("user"))
+	else:
+		if "user" in session:
+			return redirect(url_for("user"))
+		return render_template("login.html", page="login")
+
+@app.route("/user")
+def user():
+	if "user" in session:
+		user = session["user"]
+		return f"<h1>{user}</h1>"
+	else:
+		return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+	session.pop("user", None)
+	flash("You have been logged out!", "info")
+	return redirect(url_for("login"))
 
 if __name__ == "__main__":
 	app.run(debug=True)
